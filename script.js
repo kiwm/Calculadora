f36ce9574ad892b71cc52b5541fbe39d7cc91151
kiwm <pedromaltapedrozo@gmail.com>
2019-10-02 20:30:15 -0300

    atualizacao na logica do javaScrip

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-function insert(num) {
-    
-    document.form.text.value = document.form.text.value + num
+var num
+var termos = [undefined, undefined, undefined]
+var displayBuffer = ""
+var resultado = undefined;
+
+function pressNum(btn) {
+    num = num.concat(btn.innerHTML)
+    showDisplay(btn.innerHTML)
 }
 
-function equals() {
-    document.form.text.value = eval(document.form.text.value)
+function pressOperator(op) {
+    termos[0] = num
+    termos[1] = op.innerHTML
+    showDisplay(op.innerHTML)
+    num = ""
 }
 
-function clean() {
-    document.form.text.value = "";
+function pressEqual() {
+    termos[2] = num
+    switch(termos[1]) {
+        case '+':
+            resultado = Number(termos[0]) + Number(termos[2])
+            break
+        case '-':
+            resultado = Number(termos[0]) - Number(termos[2])
+            break
+        case '*':
+            resultado = Number(termos[0]) * Number(termos[2])
+            break
+        case '/':
+            resultado = Number(termos[0]) / Number(termos[2])
+            break  
+    }
+    clearAll()
+    showDisplay(resultado)
+}
+
+function clearAll() {
+    displayBuffer = ""
+    var display = document.getElementById('display')
+    display.value = displayBuffer
+    num = 0;
+}
+
+function showDisplay(cont) {
+displayBuffer = displayBuffer.concat(cont)
+var display = document.getElementById('display')
+display.value = displayBuffer
 }
